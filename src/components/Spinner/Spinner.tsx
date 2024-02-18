ca9306015fb047c17e63bd282a1889cc4d624067
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-02-18 18:17:59 +0600

    Add spinner to home page

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -2,7 +2,7 @@ const Spinner = () => {
   return (
     <div className='d-flex justify-content-center'>
       <div
-        className='spinner-border text-light'
+        className='spinner-border'
         style={{ width: '3rem', height: '3rem' }}
         role='status'
       >
--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -3,13 +3,16 @@ import MealItem from '../../components/MealItem/MealItem';
 import { useCallback, useEffect, useState } from 'react';
 import axiosApi from '../../axiosApi';
 import { ApiMeals, Meal } from '../../types';
+import Spinner from '../../components/Spinner/Spinner';
 
 const Home = () => {
   const [meals, setMeals] = useState<Meal[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
   const totalCalories = meals.reduce((acc, meal) => acc + meal.calories, 0);
 
   const fetchMeals = useCallback(async () => {
     try {
+      setIsLoading(true);
       const response = await axiosApi.get<ApiMeals | null>('/meals.json');
       const meals = response.data;
       if (meals) {
@@ -20,8 +23,11 @@ const Home = () => {
           }))
         );
       }
+      setIsLoading(false);
     } catch (error) {
       console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   }, []);
 
@@ -34,26 +40,47 @@ const Home = () => {
     void fetchMeals();
   }, [fetchMeals]);
 
+  let mealList = (
+    <div className='mt-5'>
+      <Spinner />
+    </div>
+  );
+
+  if (meals.length > 0 && !isLoading) {
+    mealList = (
+      <>
+        {meals.map((meal) => (
+          <MealItem
+            key={meal.id}
+            id={meal.id}
+            time={meal.time}
+            name={meal.name}
+            calories={meal.calories}
+            deleteMeal={deleteMeal}
+          />
+        ))}
+      </>
+    );
+  } else if (meals.length === 0 && !isLoading) {
+    mealList = (
+      <h2 className='text-center mt-5'>
+        You don't have any meals. <Link to={'/meals/new'}>Click here</Link> to
+        add your meal!
+      </h2>
+    );
+  }
+
   return (
     <div className='container'>
       <div className='d-flex align-items-center justify-content-between py-4'>
-        <p className='fs-5'>
+        <p className='fs-5 m-0'>
           Total calories: <span className='fw-bold'>{totalCalories} kcal</span>
         </p>
         <Link to={'/meals/new'} className='btn btn-primary'>
           Add new meal
         </Link>
       </div>
-      {meals.map((meal) => (
-        <MealItem
-          key={meal.id}
-          id={meal.id}
-          time={meal.time}
-          name={meal.name}
-          calories={meal.calories}
-          deleteMeal={deleteMeal}
-        />
-      ))}
+      {mealList}
     </div>
   );
 };
