965ca5f8c68eeace69b5b3e63bd8eea58b7d3505
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-02-18 17:17:46 +0600

    Create Spinner component

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,15 @@
+const Spinner = () => {
+  return (
+    <div className='d-flex justify-content-center'>
+      <div
+        className='spinner-border text-light'
+        style={{ width: '3rem', height: '3rem' }}
+        role='status'
+      >
+        <span className='visually-hidden'>Loading...</span>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
